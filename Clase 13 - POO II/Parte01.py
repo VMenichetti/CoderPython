31fe232462ec74439c6a4be9089b820986b6d0da
VERO MENICHETTI <ingvmenichetti@gmail.com>
2023-08-20 18:26:17 -0300

    Se modifico Parte.py

--- Clase 13 - POO II/Parte01.py
+++ Clase 13 - POO II/Parte01.py
@@ -1,59 +1,41 @@
+"""
+1. Definir la clase
+2. Definir el metodo constructor (Atributos)
+3. Dar un comportamiento a la clase (Funcion). Todos los metodos de cualquier clase reciben de manera obligatoria el "self"
+4. Definir el objeto o tipo de dato (Propio de la clase)
+"""
+
 class Persona():
 
-    def __init__(self,nombre,apellido,edad,dni):
-        self.nombre=nombre
-        self.apellido=apellido
-        self.edad=edad
-        self.dni=dni
+    def __init__(self,nom,ape,ed,ami):
+        self.nombre=nom
+        self.apellido=ape
+        self.edad=ed
+        self.amigos=ami
 
     def saluda(self):
-        return f'Hola, me llamo {self.nombre} {self.apellido}'
-
-class Empleado(Persona):
-
-    def __init__(self,nombre,apellido,edad,dni,sueldo,horario):
-        super().__init__(nombre,apellido,edad,dni)
-        # self.nombre=nombre
-        # self.apellido=apellido
-        # self.edad=edad
-        # self.dni=dni
-        self.sueldo=sueldo
-        self.horario=horario
-
-    def fichar(self,ingreso):
-        if ingreso < self.horario:
-            return f'Llegue {self.horario - ingreso} minutos temprano'
-        else:
-            return f'Llegue {ingreso - self.horario} minutos tarde'
-
-class Seguridad():
-
-    def __init__(self,nombre,apellido,edad,dni,sueldo,horario,vehiculo, arma):
-        self.nombre=nombre
-        self.apellido=apellido
-        self.edad=edad
-        self.dni=dni
-        self.sueldo=sueldo
-        self.horario=horario
-        self.vehiculo=vehiculo
-        self.arma=arma
+        return f'Hola soy {self.nombre} {self.apellido} y tengo {self.edad} años'
     
-    def saluda(self):
-        return f'Hola, me llamo {self.nombre} {self.apellido}'
-    
-    def fichar(self,ingreso):
-        if ingreso < self.horario:
-            return f'Llegue {self.horario - ingreso} minutos temprano'
-        else:
-            return f'Llegue {ingreso - self.horario} minutos tarde'
-    
-    def llamar_policia(self,mensaje):
-        return f'Llamo a la policia y les digo {mensaje}'
-    
-persona1=Persona('Mauricio','Cuello',32,123123)
-Empleado1=Empleado('Cristiano','Ronaldo',39,121212,1700,480)
-Seguridad1=Seguridad('Lionel','Messi',36,321321,1900,420, 'Fiat 600', 'AK-47')   
+    def lista_amigos(self):
+        mis_amigos=[]
+        for persona in self.amigos:
+            mis_amigos.append(persona.nombre)
+        lista_nombres=' '.join(mis_amigos)
+        return f'Soy amigo de {lista_nombres}'
+
+persona1=Persona("Roman","Riquelme",42, None)
+persona2=Persona("Lionel","Messi",36,None)
+persona3=Persona("Cristiano","Ronaldo",39,None)
+persona4=Persona("Mauricio","Cuello",32,[persona1,persona2,persona3])
+
+# print(persona1.nombre)
+# print(persona1.nombre,persona1.edad)     
+# print(persona2.apellido)
+
+# print(persona1.saluda("Veronica","Menichetti",37))
 
 print(persona1.saluda())
-print(Empleado1.saluda())
-print(Seguridad1.saluda())
+print(persona2.saluda())
+print(persona3.saluda())
+
+print(persona4.lista_amigos())
