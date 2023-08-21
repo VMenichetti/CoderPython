5739c3b95a3eb76e660f97f96892e7418cdc94af
VERO MENICHETTI <ingvmenichetti@gmail.com>
2023-08-21 17:45:39 -0300

    Se creo SEGUNDA PRE-ENTREGA

---
+++ SEGUNDA PRE-ENTREGA/Entrega2/main.py
@@ -0,0 +1,11 @@
+from paquete1.modulo1 import Cliente
+from paquete1.modulo2 import *
+
+cliente1=Cliente("Juan","profe@coder.com",30,["tecnología","moda","farmacia"])
+
+"METODO COMPRAR"
+cliente1.comprar("Laptop","Walmart")
+
+"METODO STR"
+
+print(cliente1) #__str__
