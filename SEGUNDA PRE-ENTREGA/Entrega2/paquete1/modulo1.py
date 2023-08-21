d8798a05f9a52dbb1bcf64715ee23ee4722b150b
VERO MENICHETTI <ingvmenichetti@gmail.com>
2023-08-21 18:12:35 -0300

    Se actualizo Entrega2

---
+++ SEGUNDA PRE-ENTREGA/Entrega2/paquete1/modulo1.py
@@ -0,0 +1,6 @@
+class Persona():
+    
+    def __init__(self,nom,ape,ed):
+        self.nombre=nom
+        self.apellido=ape
+        self.edad=ed
---
+++ SEGUNDA PRE-ENTREGA/Entrega2/setup.py
@@ -0,0 +1,9 @@
+setup(
+    name = "SegundaPreEntrega_VMenichetti",
+    version = "1.0",
+    description = "Segunda Pre-entrega PYTHON - CODERHOUSE",
+    author = "Verónica Menichetti",
+    author_email = "ingvmenichetti@gmail.com",
+
+    packages = ["SegundaPreEntrega_VMenichetti"]    
+)
